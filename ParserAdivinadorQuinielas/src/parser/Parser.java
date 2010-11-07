73b7711a56feb461f1e80c00ffbf6ddb2a5ac461
markel.arizaga <markel.arizaga@gmail.com>
2010-11-07 22:22:49 +0000

    Arreglado parser para que capture la temporada actual 	-Falta modificar los casos creados para que batxes este tranquilo

--- ParserAdivinadorQuinielas/src/parser/Parser.java
+++ ParserAdivinadorQuinielas/src/parser/Parser.java
@@ -11,6 +11,9 @@ import parserResultados.ParserResultados;
 
 public class Parser {
 
+	private static final int TEMPORADA_ACTUAL = 110;
+	private static final int ULTIMA_JORNADA = 10;
+	
 	public static void main(String[] args) {
 		ParserClasificacion parserC = new ParserClasificacion();
 		ParserResultados p = new ParserResultados();
@@ -18,7 +21,7 @@ public class Parser {
 
 			// Se parsean los datos de las clasificaciones
 			for (int temporada = 107; temporada < 110; temporada++) {
-				System.out.print("Extrayendo la informaci髇 de la temporada " + temporada + " [Clasificaciones]: |");
+				System.out.print("Extrayendo la informaci贸n de la temporada " + temporada + " [Clasificaciones]: |");
 				
 				FileWriter file = new FileWriter("Ficheros/ClasificacionesTemp" + temporada, false);
 		        PrintWriter writer = new PrintWriter(file);
@@ -45,7 +48,7 @@ public class Parser {
 			
 			//Se parsean los datos de los resultados
 			for (int temporada = 107; temporada < 110; temporada++) {
-				System.out.print("Extrayendo la informaci髇 de la temporada " + temporada + " [Resultados]: |");
+				System.out.print("Extrayendo la informaci贸n de la temporada " + temporada + " [Resultados]: |");
 				
 				FileWriter file = new FileWriter("Ficheros/ResultadosTemp" + temporada, false);
 	        	PrintWriter writer = new PrintWriter(file);	
@@ -60,6 +63,43 @@ public class Parser {
 			}
 			
 			System.out.println("Resultados parseados");
+			
+			//TODO Temporada actual
+			System.out.print("Extrayendo la informaci贸n de la temporada " + TEMPORADA_ACTUAL + " [Clasificaciones]: |");
+			
+			FileWriter file = new FileWriter("Ficheros/ClasificacionesTemp" + TEMPORADA_ACTUAL, false);
+	        PrintWriter writer = new PrintWriter(file);
+	        
+			parserC.resetFile(writer);
+			
+			parserC.parse("http://www.lfp.es/?tabid=154&Controltype=detcla&g=1&t=" + TEMPORADA_ACTUAL + "&j=1");
+			parserC.arreglarTildes();
+			parserC.escribirPrimeraClasificacion(writer);
+			for (int jornada = 1; jornada < ULTIMA_JORNADA; jornada++) {
+				System.out.print("-");
+				parserC.parse("http://www.lfp.es/?tabid=154&Controltype=detcla&g=1&t=" + TEMPORADA_ACTUAL + "&j=" + jornada);
+				parserC.arreglarTildes();
+				parserC.writeInfo(writer);
+				parserC.writeSeparator(writer);
+			}
+			
+			System.out.println("|");
+			
+			writer.close();
+			file.close();
+			
+			System.out.print("Extrayendo la informaci贸n de la temporada " + TEMPORADA_ACTUAL + " [Resultados]: |");
+			
+			file = new FileWriter("Ficheros/ResultadosTemp" + TEMPORADA_ACTUAL, false);
+        	writer = new PrintWriter(file);	
+        	
+        	p.parse("http://www.lfp.es/?tabid=154&Controltype=cal&g=1&t=" + TEMPORADA_ACTUAL);
+        	p.writeInfo(writer);
+        	
+        	System.out.println('|');
+        	
+        	writer.close();
+        	file.close();
 			
 			//Se crea el fichero de los casos juntando los parses de clasificaciones y resultados
 			CaseCreator creator = new CaseCreator();
