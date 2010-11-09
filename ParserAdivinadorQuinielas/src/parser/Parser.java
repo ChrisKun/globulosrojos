429532c6378f3f7b3bf2a163f512a1c3fb9ec2c6
markel.arizaga <markel.arizaga@gmail.com>
2010-11-09 12:52:37 +0000

    Proceso de parsing en distintos hilos

--- ParserAdivinadorQuinielas/src/parser/Parser.java
+++ ParserAdivinadorQuinielas/src/parser/Parser.java
@@ -12,53 +12,48 @@ import parserResultados.ParserResultados;
 public class Parser {
 
 	public static void main(String[] args) {
-		ParserClasificacion parserC = new ParserClasificacion();
-		ParserResultados p = new ParserResultados();
-		//Se cogue el separador dependiendo del sistema en el que estemos
-		//Si es Windows '\' y si es Linux '/'
-		//TODO En Linux funciona, falta probar en windows
-		String separator = System.getProperty("file.separator");
+
+		//Se crea un hilo para el parser de clafificaciones para que el proceso de parsing
+		//se lleve a cabo en paralelo (parsear clasificaciones y resultados simultaneamente)
+		ThreadClasificaciones hiloParserClasificaciones = new ThreadClasificaciones();
 		
+		//hiloParserClasificaciones.setPriority(Thread.MAX_PRIORITY);
+		
+		hiloParserClasificaciones.start();
+
+		parsearResultados();
+
+		try {//Esperar hasta que los dos parsers terminen su trabajo
+			hiloParserClasificaciones.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		// Se crea el fichero de los casos juntando los parses de
+		// clasificaciones y resultados
+		CaseCreator creator = new CaseCreator();
+		creator.juntarInfo();
+	}
+
+	private static void parsearResultados() {
+		
+		// Se cogue el separador dependiendo del sistema en el que estemos
+		// Si es Windows '\' y si es Linux '/'
+		String separator = System.getProperty("file.separator");
+
+		ParserResultados p = new ParserResultados();
 		try {
-
-			// Se parsean los datos de las clasificaciones
+			// Se parsean los datos de los resultados
 			for (int temporada = 107; temporada < 111; temporada++) {
-				System.out.print("Extrayendo la información de la temporada " + temporada + " [Clasificaciones]: |");
+				System.out.print("Extrayendo la información de la temporada "
+						+ temporada + " [Resultados]: |");
 
-				FileWriter file = new FileWriter("Ficheros"+ separator +"ClasificacionesTemp" + temporada, false);
+				FileWriter file = new FileWriter("Ficheros" + separator
+						+ "ResultadosTemp" + temporada, false);
 				PrintWriter writer = new PrintWriter(file);
 
-				parserC.resetFile(writer);
-
-				parserC.parse("http://www.lfp.es/?tabid=154&Controltype=detcla&g=1&t=" + temporada + "&j=1");
-				parserC.arreglarTildes();
-				parserC.escribirPrimeraClasificacion(writer);
-
-				int jornada = 1;
-				// Mientras haya exito parseando las clasificaciones almacenamos y guardamos en el fichero.
-				while (parserC.parse("http://www.lfp.es/?tabid=154&Controltype=detcla&g=1&t=" + temporada + "&j=" + jornada) && jornada < 38) {
-					System.out.print("-");
-					parserC.arreglarTildes();
-					parserC.writeInfo(writer);
-					parserC.writeSeparator(writer);
-					jornada++;
-				}
-
-				System.out.println("|");
-
-				writer.close();
-				file.close();
-			}
-			System.out.println("Clasificaciones parseadas");
-
-			//Se parsean los datos de los resultados
-			for (int temporada = 107; temporada < 111; temporada++) {
-				System.out.print("Extrayendo la información de la temporada " + temporada + " [Resultados]: |");
-
-				FileWriter file = new FileWriter("Ficheros"+ separator +"ResultadosTemp" + temporada, false);
-				PrintWriter writer = new PrintWriter(file);	
-
-				p.parse("http://www.lfp.es/?tabid=154&Controltype=cal&g=1&t=" + temporada);
+				p.parse("http://www.lfp.es/?tabid=154&Controltype=cal&g=1&t="
+						+ temporada);
 				p.writeInfo(writer);
 
 				System.out.println('|');
@@ -66,12 +61,66 @@ public class Parser {
 				writer.close();
 				file.close();
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-			System.out.println("Resultados parseados");
+		System.out.println("Resultados parseados");
+	}
+}
 
-			//Se crea el fichero de los casos juntando los parses de clasificaciones y resultados
-			CaseCreator creator = new CaseCreator();
-			creator.juntarInfo();
+class ThreadClasificaciones extends Thread{
+
+	@Override
+	public void run() {
+		parsearClasificaciones();
+	}
+	
+	private void parsearClasificaciones() {
+		// Se cogue el separador dependiendo del sistema en el que estemos
+		// Si es Windows '\' y si es Linux '/'
+		// TODO En Linux funciona, falta probar en windows
+		String separator = System.getProperty("file.separator");
+
+		try {
+			ParserClasificacion parserC = new ParserClasificacion();
+			// Se parsean los datos de las clasificaciones
+			for (int temporada = 107; temporada < 111; temporada++) {
+				System.err.print("Extrayendo la información de la temporada "
+						+ temporada + " [Clasificaciones]: |");
+
+				FileWriter file = new FileWriter("Ficheros" + separator
+						+ "ClasificacionesTemp" + temporada, false);
+				PrintWriter writer = new PrintWriter(file);
+
+				parserC.resetFile(writer);
+
+				parserC
+						.parse("http://www.lfp.es/?tabid=154&Controltype=detcla&g=1&t="
+								+ temporada + "&j=1");
+				parserC.arreglarTildes();
+				parserC.escribirPrimeraClasificacion(writer);
+
+				int jornada = 1;
+				// Mientras haya exito parseando las clasificaciones almacenamos
+				// y guardamos en el fichero.
+				while (parserC
+						.parse("http://www.lfp.es/?tabid=154&Controltype=detcla&g=1&t="
+								+ temporada + "&j=" + jornada)
+						&& jornada < 38) {
+					System.err.print("-");
+					parserC.arreglarTildes();
+					parserC.writeInfo(writer);
+					parserC.writeSeparator(writer);
+					jornada++;
+				}
+
+				System.err.println("|");
+
+				writer.close();
+				file.close();
+			}
+			System.err.println("Clasificaciones parseadas");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
