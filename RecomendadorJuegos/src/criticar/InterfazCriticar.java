5f105830ba058a320e82a443841c31349f3f2b45
54Cymru <54Cymru@gmail.com>
2010-12-07 18:22:06 +0000

    Interfaz Criticar arreglada

--- RecomendadorJuegos/src/criticar/InterfazCriticar.java
+++ RecomendadorJuegos/src/criticar/InterfazCriticar.java
@@ -1,7 +1,5 @@
 package criticar;
 
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -11,6 +9,8 @@ import sistema.Sistema;
 import jcolibri.cbrcore.Attribute;
 import jcolibri.cbrcore.CBRCase;
 import jcolibri.cbrcore.CBRQuery;
+import jcolibri.exception.ExecutionException;
+import jcolibri.extensions.recommendation.casesDisplay.UserChoice;
 import jcolibri.extensions.recommendation.conditionals.BuyOrQuit;
 import jcolibri.extensions.recommendation.conditionals.ContinueOrFinish;
 import jcolibri.extensions.recommendation.navigationByProposing.CriticalUserChoice;
@@ -18,6 +18,8 @@ import jcolibri.extensions.recommendation.navigationByProposing.CritiqueOption;
 import jcolibri.extensions.recommendation.navigationByProposing.DisplayCasesTableWithCritiquesMethod;
 import jcolibri.extensions.recommendation.navigationByProposing.queryElicitation.MoreLikeThis;
 import jcolibri.method.retrieve.RetrievalResult;
+import jcolibri.method.retrieve.FilterBasedRetrieval.FilterBasedRetrievalMethod;
+import jcolibri.method.retrieve.FilterBasedRetrieval.FilterConfig;
 import jcolibri.method.retrieve.FilterBasedRetrieval.predicates.NotEqual;
 import jcolibri.method.retrieve.FilterBasedRetrieval.predicates.QueryLess;
 import jcolibri.method.retrieve.NNRetrieval.NNConfig.SimConfigJuegos;
@@ -37,27 +39,52 @@ public class InterfazCriticar {
 		criticas.add(new CritiqueOption("Otra mecánica de juego",new Attribute("mechanics", Game.class),new NotEqual()));
 	}
 	
+    public void sequence1(CBRQuery query, FilterConfig filterConfig)  throws ExecutionException
+    {	
+		// Execute Filter
+		Collection<CBRCase> filtered = FilterBasedRetrievalMethod.filterCases(Sistema.getCBjuegosInstance().getCases(), query, filterConfig);
+		
+		// Execute NN
+		Collection<RetrievalResult> retrievedCases = NNScoringMethod.evaluateSimilarity(filtered, query, new SimConfigJuegos());
+		
+		// Select cases
+		Collection<CBRCase> selectedCases = SelectCases.selectTopK(retrievedCases, 10);
+	    	
+		// Obtain critizied query
+		CriticalUserChoice choice = DisplayCasesTableWithCritiquesMethod.displayCasesInTableWithCritiques(selectedCases, criticas, Sistema.getCBjuegosInstance().getCases());
+		
+		if(ContinueOrFinish.continueOrFinish(choice))
+		    sequence2(choice.getSelectedCaseAsQuery(), choice);
+		else
+		    sequence3(choice, selectedCases);
+    }
+    
+    public void sequence2(CBRQuery query, CriticalUserChoice cuc) throws ExecutionException
+    {
+		// Replaze current query with the critizied one
+		MoreLikeThis.moreLikeThis(query, cuc.getSelectedCase());
+		sequence1(query, cuc.getFilterConfig());
+    }
+    
+    public void sequence3(UserChoice choice, Collection<CBRCase> retrievedCases)  throws ExecutionException
+    {
+		if(BuyOrQuit.buyOrQuit(choice))
+		    System.out.println("Finish - User Buys: "+choice.getSelectedCase());
+		
+		else
+		    System.out.println("Finish - User Quits");
+    }
+	
 	public void show() {
-		boolean fin = false;
-		CBRQuery query = null;
-		while (!fin) {
-			CriticalUserChoice choice = DisplayCasesTableWithCritiquesMethod.displayCasesInTableWithCritiques(selectedCases, criticas, Sistema.getCBjuegosInstance().getCases());
-			if(ContinueOrFinish.continueOrFinish(choice)){
-				query = choice.getSelectedCaseAsQuery();
-				MoreLikeThis.moreLikeThis(query, choice.getSelectedCase());
-				// No tiene filtrado
-				// TODO Queda el simConfig (null)
-				Collection<RetrievalResult> retrievedCases = NNScoringMethod.evaluateSimilarity(Sistema.getCBjuegosInstance().getCases(), query, new SimConfigJuegos());
-				selectedCases = SelectCases.selectTopK(retrievedCases, 10);
-			}
-			else {
-				fin = true;
-				if(BuyOrQuit.buyOrQuit(choice))
-				    System.out.println("Finish - User Buys: " + choice.getSelectedCase());
-				
-				else
-				    System.out.println("Finish - User Quits");
-		    }
+		CriticalUserChoice choice = DisplayCasesTableWithCritiquesMethod.displayCasesInTableWithCritiques(selectedCases, criticas, Sistema.getCBjuegosInstance().getCases());
+		
+		try {
+		if(ContinueOrFinish.continueOrFinish(choice))
+				sequence2(choice.getSelectedCaseAsQuery(), choice);
+		else
+		    sequence3(choice, selectedCases);
+		} catch (ExecutionException e) {
+			e.printStackTrace();
 		}
 	}
 }
