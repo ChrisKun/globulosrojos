eee36b5db62b1aafede8822f96104e83c315859f
markel.arizaga <markel.arizaga@575c68f8-a00c-823f-ccef-862c3e3916a0>
2011-01-16 12:32:52 +0000

    Actualizado News Description

--- trunk/Recuperacion de informacion/src/recuperaciondeinformacion/utils/representation/NewsDescription.java
+++ trunk/Recuperacion de informacion/src/recuperaciondeinformacion/utils/representation/NewsDescription.java
@@ -9,96 +9,143 @@ import jcolibri.datatypes.Text;
 import jcolibri.extensions.textual.IE.representation.IEText;
 
 public class NewsDescription implements CaseComponent {
-	
-	String id;
-	IEText text;
-	Text title;
-	Collection<String> nombres;
-	Collection<String> verbos;
-	
-	// Características
-	ArrayList<String> Politicos = new ArrayList<String>();
-	
-	
-	public ArrayList<String> getPoliticos() {
-		return Politicos;
-	}
 
-	public void setPoliticos(ArrayList<String> politicos) {
-		Politicos = politicos;
-	}
+    String id;
+    IEText text;
+    Text title;
+    Collection<String> nombres;
+    Collection<String> verbos;
+    // Caracterï¿½sticas
+    ArrayList<String> Politicos = new ArrayList<String>();
+    ArrayList<String> JugadoresRealMadrid = new ArrayList<String>();
+    ArrayList<String> JugadoresBarcelona = new ArrayList<String>();
+    ArrayList<String> PresidentesFutbol = new ArrayList<String>();
+    ArrayList<String> EntrenadoresFutbol = new ArrayList<String>();
+    ArrayList<String> JugadoresTenis = new ArrayList<String>();
+    ArrayList<String> CorredoresFormula1 = new ArrayList<String>();
+    ArrayList<String> Paises = new ArrayList<String>();
+    ArrayList<String> Bancos = new ArrayList<String>();
 
+    public ArrayList<String> getBancos() {
+        return Bancos;
+    }
 
+    public void setBancos(ArrayList<String> Bancos) {
+        this.Bancos = Bancos;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public ArrayList<String> getCorredoresFormula1() {
+        return CorredoresFormula1;
+    }
 
+    public void setCorredoresFormula1(ArrayList<String> CorredoresFormula1) {
+        this.CorredoresFormula1 = CorredoresFormula1;
+    }
 
+    public ArrayList<String> getEntrenadoresFutbol() {
+        return EntrenadoresFutbol;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setEntrenadoresFutbol(ArrayList<String> EntrenadoresFutbol) {
+        this.EntrenadoresFutbol = EntrenadoresFutbol;
+    }
 
+    public ArrayList<String> getJugadoresBarcelona() {
+        return JugadoresBarcelona;
+    }
 
+    public void setJugadoresBarcelona(ArrayList<String> JugadoresBarcelona) {
+        this.JugadoresBarcelona = JugadoresBarcelona;
+    }
 
-	public IEText getText() {
-		return text;
-	}
+    public ArrayList<String> getJugadoresRealMadrid() {
+        return JugadoresRealMadrid;
+    }
 
+    public void setJugadoresRealMadrid(ArrayList<String> JugadoresRealMadrid) {
+        this.JugadoresRealMadrid = JugadoresRealMadrid;
+    }
 
+    public ArrayList<String> getJugadoresTenis() {
+        return JugadoresTenis;
+    }
 
-	public void setText(IEText text) {
-		this.text = text;
-	}
+    public void setJugadoresTenis(ArrayList<String> JugadoresTenis) {
+        this.JugadoresTenis = JugadoresTenis;
+    }
 
+    public ArrayList<String> getPaises() {
+        return Paises;
+    }
 
+    public void setPaises(ArrayList<String> Paises) {
+        this.Paises = Paises;
+    }
 
-	public Text getTitle() {
-		return title;
-	}
+    public ArrayList<String> getPresidentesFutbol() {
+        return PresidentesFutbol;
+    }
 
+    public void setPresidentesFutbol(ArrayList<String> PresidentesFutbol) {
+        this.PresidentesFutbol = PresidentesFutbol;
+    }
 
+    public ArrayList<String> getPoliticos() {
+        return Politicos;
+    }
 
-	public void setTitle(Text title) {
-		this.title = title;
-	}
-	
-	public Collection<String> getNombres() {
-		return nombres;
-	}
+    public void setPoliticos(ArrayList<String> politicos) {
+        Politicos = politicos;
+    }
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setNombres(Collection<String> nombres) {
-		this.nombres = nombres;
-	}
+    public IEText getText() {
+        return text;
+    }
 
+    public void setText(IEText text) {
+        this.text = text;
+    }
 
+    public Text getTitle() {
+        return title;
+    }
 
-	public Collection<String> getVerbos() {
-		return verbos;
-	}
+    public void setTitle(Text title) {
+        this.title = title;
+    }
 
+    public Collection<String> getNombres() {
+        return nombres;
+    }
 
+    public void setNombres(Collection<String> nombres) {
+        this.nombres = nombres;
+    }
 
-	public void setVerbos(Collection<String> verbos) {
-		this.verbos = verbos;
-	}
+    public Collection<String> getVerbos() {
+        return verbos;
+    }
 
+    public void setVerbos(Collection<String> verbos) {
+        this.verbos = verbos;
+    }
 
+    @Override
+    public String toString() {
+        return "NewsDescription [\n\tid=" + id + "\n\ttitle=" + title + "\n\ttext=" + text
+                + "\n\tnombres=" + nombres + "\n\tverbos=" + verbos + "\n\tpoliticos=" + Politicos + "]";
+    }
 
-	@Override
-	public String toString() {
-		return "NewsDescription [\n\tid=" + id + "\n\ttitle=" + title + "\n\ttext="	+ text +
-								"\n\tnombres=" + nombres + "\n\tverbos=" + verbos + "\n\tpoliticos=" + Politicos + "]";
-	}
-
-
-
-	@Override
-	public Attribute getIdAttribute() {
-		return new Attribute("id", this.getClass());
-	}
-
+    @Override
+    public Attribute getIdAttribute() {
+        return new Attribute("id", this.getClass());
+    }
 }
