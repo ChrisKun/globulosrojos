eb084be6f80a478be01bedf58be58bf4a8ff83e4
batxes <batxes@575c68f8-a00c-823f-ccef-862c3e3916a0>
2011-01-26 14:18:39 +0000

    ainadiendo test, parte 1

--- Recuperacion de informacion/src/recuperaciondeinformacion/test/Test.java
+++ Recuperacion de informacion/src/recuperaciondeinformacion/test/Test.java
@@ -7,6 +7,7 @@ package recuperaciondeinformacion.test;
 
 import jcolibri.evaluation.Evaluator;
 import jcolibri.evaluation.evaluators.HoldOutEvaluator;
+import jcolibri.exception.ExecutionException;
 import recuperaciondeinformacion.RecuperadorDeInformacion;
 
 /**
@@ -15,23 +16,43 @@ import recuperaciondeinformacion.RecuperadorDeInformacion;
  */
 public class Test {
 
-    public static void HoldOutEvaluation()
-    {
-            //SwingProgressBar shows the progress
-    jcolibri.util.ProgressController.clear();
-    jcolibri.util.ProgressController.register(new jcolibri.test.main.SwingProgressBar(), HoldOutEvaluator.class);
-
-    // Example of the Hold-Out evaluation
-            HoldOutEvaluator eval = new HoldOutEvaluator();
-            eval.init(new RecuperadorDeInformacion());
-            eval.HoldOut(15, 1);
-
-
-            System.out.println(Evaluator.getEvaluationReport());
-            jcolibri.evaluation.tools.EvaluationResultGUI.show(Evaluator.getEvaluationReport(), "Recuperador de informacion - Evaluation", false);
-    }
+	public static void prueba() {
+		Thread t = new Thread(new Runnable() {
+			public void run()
+			{
+				RecuperadorDeInformacion test = new RecuperadorDeInformacion();
+				
+				try {
+					test.configure();
+					test.preCycle();		
+				
+					test.HoldOutEvaluation();
+					
+					java.util.Vector<Double> vec = Evaluator.getEvaluationReport().getSeries("Errores");
+					double avg = 0.0;
+					for (Double d: vec)
+						avg+=d;
+					avg=avg/(double)Evaluator.getEvaluationReport().getNumberOfCycles();
+					avg = 1- avg;
+					Evaluator.getEvaluationReport().putOtherData("Media errores", Double.toString(avg));
+						
+					java.util.Vector<Double> vec2 = Evaluator.getEvaluationReport().getSeries("Confianza");
+					avg = 0.0;
+					for (Double d: vec2)
+						avg+=d;
+					avg=avg/(double)Evaluator.getEvaluationReport().getNumberOfCycles();
+					Evaluator.getEvaluationReport().putOtherData("Media confianza", Double.toString(avg));
+						
+					System.out.println(Evaluator.getEvaluationReport());
+					jcolibri.evaluation.tools.EvaluationResultGUI.show(Evaluator.getEvaluationReport(), "Evaluacion Recuperador Informacion", false);
+				} catch (ExecutionException e) {
+				}
+			}
+		});
+		t.start();
+	}
 
     public static void main(String[] args) {
-        Test.HoldOutEvaluation();
+        Test.prueba();
     }
 }
