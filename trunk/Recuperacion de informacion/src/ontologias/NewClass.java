b72b58f6fd903ca1358fd115ae2ab02e1a0e701e
54Cymru <54Cymru@575c68f8-a00c-823f-ccef-862c3e3916a0>
2011-02-26 17:24:51 +0000

    Subida la clase de prueba para ontologías

---
+++ trunk/Recuperacion de informacion/src/ontologias/NewClass.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ontologias;
+
+import es.ucm.fdi.gaia.ontobridge.OntoBridge;
+import es.ucm.fdi.gaia.ontobridge.OntologyDocument;
+import es.ucm.fdi.gaia.ontobridge.test.gui.PnlConceptsTree;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sergio
+ */
+public class NewClass {
+    public static void main (String[] args) {
+        OntoBridge ob = new OntoBridge();
+        ob.initWithPelletReasoner();
+
+        OntologyDocument mainOnto = new OntologyDocument("","file:Ontologia.owl");
+
+        ArrayList<OntologyDocument> subOntologies = new ArrayList<OntologyDocument>();
+
+        ob.loadOntology(mainOnto, subOntologies, false);
+
+        javax.swing.JFrame window = new javax.swing.JFrame(mainOnto.getURL());
+        PnlConceptsTree tree = new PnlConceptsTree(ob);
+        window.getContentPane().add(tree);
+        window.pack();
+        window.setSize(300, 600);
+        window.setVisible(true);
+    }
+}
