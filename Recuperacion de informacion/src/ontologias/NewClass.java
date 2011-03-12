1b6bdd80fec01a5d0f5b8b6db2d63791e18e2607
markel.arizaga <markel.arizaga@575c68f8-a00c-823f-ccef-862c3e3916a0>
2011-03-12 12:34:21 +0000

    Añado clase de menu contextual. Segun que tipo de la ontologia pulsado saldra un menu distinto. Ejemplo de uso en el Mouse Pressed de PanelArbolInstancias.

--- Recuperacion de informacion/src/ontologias/NewClass.java
+++ Recuperacion de informacion/src/ontologias/NewClass.java
@@ -3,6 +3,7 @@ package ontologias;
 import es.ucm.fdi.gaia.ontobridge.OntoBridge;
 import es.ucm.fdi.gaia.ontobridge.OntologyDocument;
 import java.util.ArrayList;
+import ontologias.interfaz.panel.PanelArbolClasesInstancias;
 import ontologias.interfaz.panel.PanelArbolPropiedades;
 import ontologias.interfaz.panel.PanelListaNoticias;
 
@@ -22,10 +23,10 @@ public class NewClass {
         ob.loadOntology(mainOnto, subOntologies, false);
 
         javax.swing.JFrame window = new javax.swing.JFrame(mainOnto.getURL());
-        //PanelListaNoticias tree = new PanelListaNoticias(ob, );
-//        window.getContentPane().add(tree);
-//        window.pack();
-//        window.setSize(300, 600);
-//        window.setVisible(true);
+        PanelArbolClasesInstancias tree = new PanelArbolClasesInstancias(ob, null);
+        window.getContentPane().add(tree);
+        window.pack();
+        window.setSize(300, 600);
+        window.setVisible(true);
     }
 }
