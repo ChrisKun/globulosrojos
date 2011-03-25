250cf542eb806d191e1139db71eb1fa93c82a05a
markel.arizaga <markel.arizaga@575c68f8-a00c-823f-ccef-862c3e3916a0>
2011-03-25 17:20:25 +0000

    ...

---
+++ trunk/SBTournament-2011/src/grupo14/manager/Mourinho.java
@@ -0,0 +1,20 @@
+package grupo14.manager;
+
+import teams.rolebased.TeamManager;
+import teams.rolebased.WorldAPI;
+
+public class Mourinho extends TeamManager {
+
+	@Override
+	public int configure() {
+		// TODO Auto-generated method stub
+		return WorldAPI.ROBOT_OK;
+	}
+
+	@Override
+	public int takeStep() {
+		// TODO Auto-generated method stub
+		return WorldAPI.ROBOT_OK;
+	}
+
+}
