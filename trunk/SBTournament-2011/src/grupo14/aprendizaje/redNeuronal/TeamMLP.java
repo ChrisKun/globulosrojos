c75f71bef1be6fdb3c26d88dda5b6f49fb76ca88
54Cymru@gmail.com <54Cymru@gmail.com@575c68f8-a00c-823f-ccef-862c3e3916a0>
2011-05-04 20:03:38 +0000

    Añadido el diseño del equipo en la red neuronal. Añadidos métodos para guardar y leer perceptrones de ficheros.

---
+++ trunk/SBTournament-2011/src/grupo14/aprendizaje/redNeuronal/TeamMLP.java
@@ -0,0 +1,11 @@
+package grupo14.aprendizaje.redNeuronal;
+
+import grupo14.aprendizaje.redNeuronal.players.PlayerMLP;
+
+import java.util.HashMap;
+
+public class TeamMLP {
+	
+	/** Tabla de jugadores de un equipo. */
+	private HashMap<String, PlayerMLP> players;
+}
