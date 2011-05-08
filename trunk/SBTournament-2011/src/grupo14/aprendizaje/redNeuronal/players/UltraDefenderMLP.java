7ff8142378fe22387088269ad3326280757ae10b
54Cymru@gmail.com <54Cymru@gmail.com@575c68f8-a00c-823f-ccef-862c3e3916a0>
2011-05-08 19:38:44 +0000

    Añadido el sistema de aprendizaje completo y un defensor de prueba para la red neuronal.

---
+++ trunk/SBTournament-2011/src/grupo14/aprendizaje/redNeuronal/players/UltraDefenderMLP.java
@@ -0,0 +1,60 @@
+package grupo14.aprendizaje.redNeuronal.players;
+
+import java.util.ArrayList;
+
+import EDU.gatech.cc.is.util.Vec2;
+import grupo14.aprendizaje.redNeuronal.log.LogEntry;
+import grupo14.aprendizaje.redNeuronal.log.PlayerInfo;
+
+public class UltraDefenderMLP extends PlayerMLP {
+	
+	/** Determina si la jugada es buena para ser aprendida 
+	 * @param oldState Estado del juego en el pasado.
+	 * @param newState Estado del juego en el presente. 
+	 * @param fieldSide Lado del campo del jugador.
+	 * @return true si ha habido una mejora en el cambio de estado, false en otro caso. */
+	protected boolean isGoodMove(LogEntry oldState, LogEntry newState, int fieldSide) {
+		/* Es una buena jugada si:
+		 *   · La pelota pasa de nuestro campo al campo contrario.
+		 *   · La pelota está en nuestro campo y avanza una determinada cantidad en X */
+		Vec2 oldBallPosition = new Vec2(oldState.getBallInfo().getPositionX(), oldState.getBallInfo().getPositionY());
+		Vec2 newBallPosition = new Vec2(newState.getBallInfo().getPositionX(), newState.getBallInfo().getPositionY());
+		
+		if ((oldBallPosition.x * fieldSide > 0 && newBallPosition.x * fieldSide < 0) ||
+			(oldBallPosition.x * fieldSide > 0 && (newBallPosition.x - oldBallPosition.x) * fieldSide < 0.3)) 
+			return true;
+		else return false;	
+	}
+	
+	/** Determina el jugador que se quiere tomar como referencia para aprender la jugada. 
+	 * @param oldState Estado del juego.
+	 * @return id del jugador que se toma como referencia. */
+	protected int getReferencePlayer(LogEntry state, int fieldSide) {
+		/* El jugador de referencia es el que esté más retrasado en su campo
+		 * (sin contar al portero, es decir, el segundo más retrasado) */
+		int id1 = -1; // Id del jugador más retrasado (lolazo)
+		int id2 = -1; // Id del segundo jugador más retrasado (lolapong)
+		double xPos1 = Double.MIN_VALUE; // Posición del jugador más retrasado (facepalm)
+		double xPos2 = Double.MIN_VALUE; // Posición del segundo jugador más retrasado (u serious?1)
+		
+		ArrayList<PlayerInfo> team;
+		if (fieldSide == -1)
+			team = state.getWestTeamInfo();
+		else team = state.getEastTeamInfo();
+		
+		for (PlayerInfo player : team)
+			if (player.getPositionX() * fieldSide > xPos1) {
+				id2 = id1;
+				xPos2 = xPos1;
+				id1 = player.getRobotId();
+				xPos1 = player.getPositionX() * fieldSide;
+			}
+			else if (player.getPositionX() * fieldSide > xPos2) {
+				id2 = player.getRobotId();
+				xPos2 = player.getPositionX() * fieldSide;
+			}
+		
+		return id2;
+	}
+
+}
