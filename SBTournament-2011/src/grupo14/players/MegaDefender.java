6eedc33dd8f2271669533b14865f0520db5eae0a
54cymru <54cymru@575c68f8-a00c-823f-ccef-862c3e3916a0>
2011-05-12 14:53:35 +0000

    Arreglado el problemilla con los fieldSide, la red ya es independiente del lado del campo en el que juega.

--- SBTournament-2011/src/grupo14/players/MegaDefender.java
+++ SBTournament-2011/src/grupo14/players/MegaDefender.java
@@ -1,4 +1,5 @@
 package grupo14.players;
+import grupo14.aprendizaje.redNeuronal.players.UltraDefenderMLP;
 import grupo14.states.Catenaccio;
 import grupo14.states.Heroica;
 import grupo14.states.JuegoBrusco;
@@ -18,12 +19,18 @@ public class MegaDefender  extends Role{
 
 	public MatchStateUtils matchStateUtils;
 	String role="megaDefensor";
+	UltraDefenderMLP mlp;
 	
 	@Override
 	public int configure() {
 		//Para cuando se quiera ver el nombre de los jugadores en el simulador
 		worldAPI.setDisplayString(role);
 		this.matchStateUtils = new MatchStateUtils();
+		
+		// Inicialización de la red neuronal para el ultra defensor
+		mlp = new UltraDefenderMLP();
+		mlp.readFromFile("training/MLP/UltraDefender");
+		
 		return WorldAPI.ROBOT_OK;
 	}
 
@@ -38,6 +45,9 @@ public class MegaDefender  extends Role{
 		matchStateUtils.setMatchState(new PosesionContrarioEnSuCampo());
 		//getMatchState();
 		matchStateUtils.matchState.accionARealizar(worldAPI,role);
+		
+		// El ultradefender muestra el movimiento que le aconseja la red neuronal + confianza (testing)
+		worldAPI.setDisplayString(mlp.getNextMove(worldAPI));
 		
 		return WorldAPI.ROBOT_OK;
 	}
