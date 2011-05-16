a425306739ef6aa034ec1044623dd35aa45a9e61
batxes <batxes@575c68f8-a00c-823f-ccef-862c3e3916a0>
2011-05-16 14:24:56 +0000

    accion del portero implementado

--- trunk/SBTournament-2011/src/grupo14/players/Goalkeeper.java
+++ trunk/SBTournament-2011/src/grupo14/players/Goalkeeper.java
@@ -1,5 +1,6 @@
 package grupo14.players;
 
+import EDU.gatech.cc.is.util.Vec2;
 import grupo14.states.PosesionContrarioEnSuCampo;
 import grupo14.team.Ordenes;
 import grupo14.utils.MatchStateUtils;
@@ -27,6 +28,8 @@ public class Goalkeeper extends X{
 
 	@Override
 	public int takeStep() {
+		//accion normal, ni neuronal ni cbr
+		accionPortero();
 		
 		//Ver si hay nueva orden de la red neuronal
 		//if( HAY NUEVA ORDEN DE RED NEURONAL  )
@@ -48,4 +51,43 @@ public class Goalkeeper extends X{
 	{
 		return this.worldAPI;
 	}
+	public void accionPortero ()
+	{
+		double ROBOT_RADIUS = 0.6;
+		Vec2 move = new Vec2(0,0);
+		// if the ball is behind me try to kick it out
+		if( worldAPI.getBall().x * worldAPI.getFieldSide() > 0)
+		{
+			move.sett( worldAPI.getBall().t);
+			move.setr( 1.0);
+		}
+
+		// if i'm outside the goal area go back toward the goal
+		else if( (Math.abs(worldAPI.getOurGoal().x) > ROBOT_RADIUS * 1.4) ||
+			 (Math.abs(worldAPI.getOurGoal().y) > ROBOT_RADIUS * 4.25) )
+
+		{
+			move.sett( worldAPI.getOurGoal().t);
+			move.setr( 1.0);
+		}
+
+		// stay between the ball and the goal
+		else
+		{
+			if( worldAPI.getBall().y > 0)
+				move.sety( 7);
+			else
+				move.sety( -7);
+
+			move.setx( (double)worldAPI.getFieldSide());
+
+			if( Math.abs( worldAPI.getBall().y) < ROBOT_RADIUS * 0.15)
+				move.setr( 0.0);
+			else
+				move.setr( 1.0);
+		}
+		worldAPI.setSpeed(1.0);
+	}
+	
+	
 }
